00f2e5482babb4d87ac91b614e73a2061615840f
Davieas <davialexandresantana7@gmail.com>
2023-02-21 16:43:54 -0300

    Industrial-Gopher

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/Davieas/Industrial-GolangCLP
+
+go 1.20
+
+require github.com/robinson/gos7 v0.0.0-20230126084723-c85e13033f3e
+
+require (
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/robinson/gos7"
+)
+
+func main() {
+	// Cria uma conexão com o CLP Siemens
+	conn := gos7.NewClient(192.168.0.100, 0x01, 0x10)
+	err := conn.Connect()
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Disconnect()
+
+	// Liga o motor
+	err = conn.WriteBySymbol("DB1.DBX0.0", 1)
+	if err != nil {
+		panic(err)
+	}
+
+	// Espera 5 segundos antes de ligar a bomba
+	time.Sleep(5 * time.Second)
+
+	// Liga a bomba
+	err = conn.WriteBySymbol("DB1.DBX2.0", 1)
+	if err != nil {
+		panic(err)
+	}
+
+	// Lê o valor do sensor de nível
+	sensorValue, err := conn.ReadFloatBySymbol("DB1.DBD4")
+	if err != nil {
+		panic(err)
+	}
+
+	// Verifica se o nível está alto
+	if sensorValue >= 100.0 {
+		fmt.Println("Nível alto alcançado!")
+	} else {
+		fmt.Println("Nível baixo ainda.")
+	}
+}
